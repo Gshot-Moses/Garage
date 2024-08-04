1386baa962f0d69f604f571d2eca507d926d7d37
Moussa Sani <moussa.sani@mh-or.com>
2024-08-04 01:13:53 +0100

    Sign in api service

---
+++ lib/features/signin/sign_in_api.chopper.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_in_api.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _$SignInService extends SignInService {
+  _$SignInService([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final Type definitionType = SignInService;
+
+  @override
+  Future<Response<dynamic>> login(Map<String, dynamic> data) {
+    final Uri $url = Uri.parse('/signin');
+    final $body = data;
+    final Request $request = Request(
+      'POST',
+      $url,
+      client.baseUrl,
+      body: $body,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+}
---
+++ lib/features/signin/sign_in_api.dart
@@ -0,0 +1,12 @@
+import 'package:chopper/chopper.dart';
+
+part 'sign_in_api.chopper.dart';
+
+@ChopperApi(baseUrl: "")
+abstract class SignInService extends ChopperService {
+
+  static SignInService create({ChopperClient? client}) => _$SignInService(client);
+
+  @Post(path: "/signin")
+  Future<Response> login(@Body() Map<String, dynamic> data);
+}
