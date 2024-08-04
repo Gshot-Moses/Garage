0178699107cdd81154034c733dee45d98c423ff8
Moussa Sani <moussa.sani@mh-or.com>
2024-08-05 00:08:44 +0100

    Add generated models.g.dart

---
+++ lib/features/signin/models/models.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
+    LoginResponse(
+      username: json['username'] as String,
+      email: json['email'] as String,
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+    );
+
+Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
+    <String, dynamic>{
+      'username': instance.username,
+      'email': instance.email,
+      'accessToken': instance.accessToken,
+      'refreshToken': instance.refreshToken,
+    };
